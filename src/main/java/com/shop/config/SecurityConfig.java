47618b7bed5d2ada54e28c2eccfb8400ccc32323
황승아 <mina789@naver.com>
2024-05-24 23:26:09 +0900

    회원가입 기능 추가

---
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.shop.config;
+
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+    @Autowired
+    MemberService memberService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin((formLogin) ->
+                        formLogin.loginPage("/members/login/")
+                                .defaultSuccessUrl("/")
+                                .usernameParameter("email")
+                )
+                .logout((logout) ->
+                        logout.logoutSuccessUrl("/")
+                                .invalidateHttpSession(true)
+                );
+
+        http.authorizeRequests()
+                .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+                .requestMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
