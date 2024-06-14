3eaf6d1a697ad252682b54b6f242454fd3254e04
황승아 <mina789@naver.com>
2024-06-14 16:02:42 +0900

    상품 관리하기

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> , QuerydslPredicateExecutor<Item> {
+public interface ItemRepository extends JpaRepository<Item, Long> , QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
     List<Item> findByItemNm(String itemNm);
 
     @Query("select i from  Item i where i.itemDetail like %:itemDetail% order by i.price desc")
