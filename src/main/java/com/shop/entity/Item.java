4eafef8f351fdaeb70e31502a9a48c68efd7f2f3
황승아 <mina789@naver.com>
2024-06-16 21:32:40 +0900

    주문 취소

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -49,4 +49,8 @@ public class Item extends BaseEntity  {
         this.stockNumber = restStock;
     }
 
+    public void addStock(int stockNumber){
+        this.stockNumber += stockNumber;
+    }
+
 }
