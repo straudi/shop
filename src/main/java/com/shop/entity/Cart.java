801331a05226d27ec6cf4f776f2614d55c725740
황승아 <mina789@naver.com>
2024-06-16 22:29:08 +0900

    장바구니 담기

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -1,5 +1,6 @@
 package com.shop.entity;
 
+import com.shop.dto.CartItemDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,4 +20,10 @@ public class Cart extends BaseEntity  {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    public static Cart createCart(Member member){
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 }
